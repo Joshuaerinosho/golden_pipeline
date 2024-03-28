a6b1c40d43e993d6ef2603fe1729799cf67dc045
Joshua Erinosho <joshuaerinosho@gmail.com>
2024-03-28 09:48:57 +0100

    Generate goldens

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,6 +14,12 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile(
+        '../goldens/test.png',
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
